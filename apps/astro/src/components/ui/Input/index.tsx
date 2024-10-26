d9b1e392c527d4a96cf4f217baf5e983755a5d59
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-10-26 17:48:35 +0200

    add Input ui element

---
+++ apps/astro/src/components/ui/Input/index.tsx
@@ -0,0 +1,27 @@
+import type { FieldErrors } from 'react-hook-form'
+import styles from './Input.module.scss'
+import Error from '@/src/components/ui/Error'
+import Textarea from './Textarea'
+
+type Props = {
+  register: {
+    name: string
+  }
+  label: string
+  isTextarea?: boolean
+  errors: FieldErrors;
+} & React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>
+
+export default function Input({ register, label, isTextarea, errors, ...props }: Props) {
+  const Element = isTextarea ? Textarea : 'input'
+
+  return (
+    <label className={styles.Input}>
+      <p className={styles.label}>
+        {label}
+        <Error error={errors[register.name]?.message?.toString()} />
+      </p>
+      <Element {...register} {...props} aria-invalid={!!errors[register.name]} />
+    </label>
+  )
+}
