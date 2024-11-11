3c977118effd1bbe89f48fb02a051f4207ec119b
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-11-11 13:02:30 +0100

    add components to Project_Collection as page builder

--- apps/sanity/schema/collectionTypes/Project_Collection.tsx
+++ apps/sanity/schema/collectionTypes/Project_Collection.tsx
@@ -179,6 +179,11 @@ export default defineType({
       }
     }),
     defineField({
+      name: 'components',
+      type: 'components',
+      title: 'Page Components',
+    }),
+    defineField({
       name: 'seo',
       type: 'seo',
       title: 'SEO',
