f878b1da9571165acf0dad9837d9856d78bb9c87
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-12-21 14:44:27 +0100

    update import in favicon

--- apps/astro/src/pages/favicon.ico.ts
+++ apps/astro/src/pages/favicon.ico.ts
@@ -1,15 +1,15 @@
 export const prerender = true;
 
 import type { APIRoute } from "astro";
-import path from "node:path";
 import sharp from "sharp";
 import ico from "sharp-ico";
-
-const favicon = path.resolve("src/assets/favicon.svg");
+import favicon from "@/src/assets/favicon.svg?raw";
 
 export const GET: APIRoute = async () => {
-  const buffer = await sharp(favicon).resize(32).toBuffer();
-  const icoBuffer = ico.encode([buffer]);
+  const processedFavicon = await sharp(Buffer.from(favicon))
+    .resize(32, 32)
+    .toBuffer();
+  const icoBuffer = ico.encode([processedFavicon]);
 
   return new Response(icoBuffer, {
     headers: { "Content-Type": "image/x-icon" },
