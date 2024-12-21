be851f0ff52459958ededba3bbadd723f563efff
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-12-21 15:20:17 +0100

    update sanity.fetch util

--- apps/astro/src/utils/sanity.fetch.ts
+++ apps/astro/src/utils/sanity.fetch.ts
@@ -1,19 +1,17 @@
 import { createClient, type QueryParams } from '@sanity/client'
 import { isPreviewDeployment } from './is-preview-deployment';
 
-const SANITY_API_TOKEN = import.meta.env.SANITY_API_TOKEN || process.env.SANITY_API_TOKEN;
+const SANITY_API_TOKEN = process.env.SANITY_API_TOKEN || import.meta.env.SANITY_API_TOKEN;
 
-if (isPreviewDeployment && !SANITY_API_TOKEN) {
-  console.warn('\x1b[33m%s\x1b[0m', "The `SANITY_API_TOKEN` environment variable is required.");
-}
+if (!SANITY_API_TOKEN) console.warn('\x1b[33m%s\x1b[0m', "The `SANITY_API_TOKEN` environment variable is required.");
 
 export const client = createClient({
   projectId: 'ozvu6nwg',
   dataset: 'production',
-  apiVersion: '2024-08-30',
+  apiVersion: '2024-12-21',
   useCdn: false,
   perspective: isPreviewDeployment ? 'previewDrafts' : 'published',
-  ...(isPreviewDeployment && { token: SANITY_API_TOKEN }),
+  token: SANITY_API_TOKEN
 })
 
 export default async function sanityFetch<QueryResponse>({
