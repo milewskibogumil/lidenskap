dfd7a5ac6a0a9d82b01f0e99a51a62a62d44da77
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-12-21 09:45:40 +0100

    update global Head for Layout and metadata.fetch util

--- apps/astro/src/utils/metadata.fetch.ts
+++ apps/astro/src/utils/metadata.fetch.ts
@@ -1,28 +1,20 @@
-
-import type { Props } from "@/src/layouts/Head.astro";
 import sanityFetch from "@/utils/sanity.fetch";
+import { type HeadProps } from '@/src/layouts/Head.astro'
 
-export default async function metadataFetch(type: string, slug?: string): Promise<Props> {
-  const filter = slug
-    ? `*[_type == '${type}' && slug.current == $slug][0]`
-    : `*[_type == "${type}"][0]`;
-
-  const seo = await sanityFetch<Props>({
+export default async function metadataFetch(slug: string): Promise<HeadProps> {
+  const seo = await sanityFetch<HeadProps>({
     query: /* groq */ `
-      ${filter} {
+      *[slug.current == $slug][0] {
         "path": slug.current,
         "title": seo.title,
         "description": seo.description,
-        "openGraphImage": {
-          "url": seo.img.asset -> url + "?w=1200",
-          "height": round(1200 / seo.img.asset -> metadata.dimensions.aspectRatio),
-        },
+        "openGraphImage": seo.img.asset -> url + "?w=1200",
       }
     `,
-    ...(slug && { params: { slug: slug } }),
-  });
-  if (!seo?.path) throw new Error(`The path for '${type}' is not specified`);
-  if (!seo?.title) throw new Error(`The title for '${type}' is not specified`);
-  if (!seo?.description) throw new Error(`The description for '${type}' is not specified`);
+    params: { slug: slug }
+  })
+  if (!seo?.path) throw new Error(`Missing required field \`path\` for slug \`${slug}\``);
+  if (!seo?.title) throw new Error(`Missing required field \`title\` for slug \`${slug}\``);
+  if (!seo?.description) throw new Error(`Missing required field \`description\` for slug \`${slug}\``);
   return seo;
 }
