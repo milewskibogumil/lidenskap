69f2f9ef8d93e20c3ae297f38feac5298adeb445
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2025-03-08 16:39:02 +0100

    update sanity.fetch

--- apps/astro/src/utils/sanity.fetch.ts
+++ apps/astro/src/utils/sanity.fetch.ts
@@ -6,9 +6,9 @@ const SANITY_API_TOKEN = process.env.SANITY_API_TOKEN || import.meta.env.SANITY_
 if (!SANITY_API_TOKEN) console.warn('\x1b[33m%s\x1b[0m', "The `SANITY_API_TOKEN` environment variable is required.");
 
 export const client = createClient({
-  projectId: 'ozvu6nwg',
+  projectId: '0qbklpoh',
   dataset: 'production',
-  apiVersion: '2024-12-21',
+  apiVersion: '2025-03-08',
   useCdn: false,
   perspective: isProductionDeployment ? 'published' : 'drafts',
   token: SANITY_API_TOKEN
